c0b74a9b5cd8f63949f0fd7e03c91282a1da7b89
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-10-28 18:27:32 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { TopBar } from "./TopBar";
+import { Register } from "./Register";
 import './App.css';
 
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
        <TopBar/>
+       <Register/>
       </div>
     );
   }
