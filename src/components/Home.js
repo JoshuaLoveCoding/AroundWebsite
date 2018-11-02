49851404437ec6023598e12a5e8d093ac971931b
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-11-01 22:48:52 -0400

    load geo location

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+import { GEO_OPTIONS } from "../constants";
+
+const TabPane = Tabs.TabPane;
 
 export class Home extends React.Component {
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        this.onSuccessLoadGeoLocation,
+        this.onFailedLoadGeoLocation,
+        GEO_OPTIONS);
+    } else {
+      /* geolocation IS NOT available */
+    }
+  }
+
+  onSuccessLoadGeoLocation = (position) => {
+    console.log(position);
+  }
+
+  onFailedLoadGeoLocation = () => {
+    console.log('Failed to load geo location');
+  }
+
   render() {
+    const operations = <Button type="primary">Create New Post</Button>;
     return (
-      <div>This is home!</div>
+      <Tabs tabBarExtraContent={operations} className="main-tabs">
+        <TabPane tab="Image Posts" key="1">Content of tab 1</TabPane>
+        <TabPane tab="Video Posts" key="2">Content of tab 2</TabPane>
+        <TabPane tab="Map" key="3">Content of tab 3</TabPane>
+      </Tabs>
     );
   }
 }
